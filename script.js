7b2604957bec973618b6b352f7257f8757d6e0c8
Valeria <152664482+VaAl08@users.noreply.github.com>
2024-01-03 20:21:07 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+// // const listaDeNumeros= [2, 3, 5, 7, 11];
+
+// // console.log(listaDeNumeros[0]);
+// // console.log(listaDeNumeros[4]);
+
+// // let numerosimpares = [1, 3, 5, 7];
+// // cantidad = numerosimpares.length;
+// // console.log("primera forma:")
+// // for(i = 0; cantidad > i; i++){
+// //     console.log(numerosimpares[i]);
+// // }
+// // console.log ("segunda forma:")
+// // for(const valor of numerosimpares){
+// //     console.log(valor);
+// // }
+
+// const listadenumeros= [2,3,5,7,11];
+// listadenumeros[2] =13;
+// console.log(listadenumeros[2]);
+
+// // listadenumeros = [];
+// // console.log(listad<>numeros.length); //muestra el tamaño
+
+// listadenumeros[0] = 100;
+// console.log(listadenumeros[0]);
+
+// // listadenumeros[1] = 102 ;
+// // listadenumeros[2] = 10 ;
+// // listadenumeros[3] = 23 ;
+// // listadenumeros[4] = -3 ;
+// console.log(listadenumeros[2]);
+
+const mixedArray = [3, 'hola mudo', 3627.5];
+cantidad = mixedArray.length;
+for(i = 0; cantidad > i; i++){
+    console.log(mixedArray[i]);
+}
+
+let questions = [
+    ['cual es la capital de peru?', 'lima'],
+    ['cuamtos millones de peruanos somos?', 33],
+    ['cual es la moneda de peru', 'soles'],
+];
+
+console.log(questions[0][0]);
+console.log(questions[0][1]);
+console.log(questions[1][0]);
+console.log(questions[1][1]);
+console.log(questions[2][0]);
+console.log(questions[2][1])
